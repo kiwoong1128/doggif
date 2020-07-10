92d5d2708c6d552337c61fb84dbb1f90898d6e31
kkim45 <kkim45@masonlive.gmu.edu>
2020-07-09 20:32:48 -0400

    async

--- scripts.js
+++ scripts.js
@@ -1,18 +1,12 @@
 const img = document.querySelector('img');
 window.onload(refresh());
 
-function refresh() {
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=F2I3Za0vGpA7VnYZUw4QcQiaOQ09xGhD&s=dogs', {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      img.src = response.data.images.original.url;
-    });
+async function refresh() {
+  const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=F2I3Za0vGpA7VnYZUw4QcQiaOQ09xGhD&s=puppies', {mode: 'cors'})
+  const dogData = await response.json();
+  img.src = dogData.data.images.original.url;
 }
 
-
 function clicked() {
-  alert('clicked');
   refresh();
 }
